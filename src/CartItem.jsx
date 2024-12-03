1607cd17e53763b39dc1ebc39b370af186eac7dd
Ngqabutho Dlamini <ngqaza.dlamz47@gmail.com>
2024-12-03 21:00:47 +0200

    Implemented reducers

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+
+    cart.forEach(item =>{
+      totalCost += (Number(item.cost) * Numbe(item.quantity))
+    });
+
+  return totalCost
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.name, item.quantity+1))
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quatity > 1){
+    dispatch(updateQuantity(item.name, item.quantity-1))
+   }else{
+    dispatch(removeItem(item.name))
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity
   };
 
   return (
